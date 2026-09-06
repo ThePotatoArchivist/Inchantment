26c540348d75538b733ab329f62d6c0f9c697a00
ThePotatoArchivist <josiahfu@gmail.com>
2026-09-06 14:20:57 -0700

    Fix renaming cost with easy anvils

--- src/main/java/archives/tater/penchant/component/EnchantmentProgress.java
+++ src/main/java/archives/tater/penchant/component/EnchantmentProgress.java
@@ -85,6 +85,9 @@ public class EnchantmentProgress {
             return new EnchantmentProgress(progress);
         }
 
+        public boolean isEmpty() {
+            return progress.isEmpty();
+        }
     }
 
     public static boolean shouldShowTooltip(Holder<Enchantment> enchantment) {
